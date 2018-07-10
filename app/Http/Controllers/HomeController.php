b6ee581cb57a7027777c662b8947d6781341b233
unknown <angellamumbe720@gmail.com>
2018-07-11 00:38:35 +0300

    m

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,8 +6,12 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
+    public function _construct()
+    {
+        $this->middleware('auth');
+    }
     public function index()
     {
-        return view('/home');
+        return view('home');
     }
 }
