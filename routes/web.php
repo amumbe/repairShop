fb000e8a24b80f5d9f8625457ef1cc27127d3688
amumbe <32109516+amumbe@users.noreply.github.com>
2018-07-11 13:05:25 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,4 @@ Route::get('/listShop/{id}', 'ListShopsController@show');
 Route::post('login', 'API\UserController@login');
 Route::post('register', 'API\UserController@register');
 
-Route::group(['middleware' => 'auth:api']), function (){
-Route::post('details', 'API\UserController@details');
-});
+
