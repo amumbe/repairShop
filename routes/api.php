af9275a3b5d12c1761204b4ea5d1d4392bde1e9e
amumbe <32109516+amumbe@users.noreply.github.com>
2018-07-11 13:03:52 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -30,7 +30,5 @@ Route::get('/instructor/{id}', 'ListShopsController@show');
 Route::post('login', 'API\UserController@login');
 Route::post('register', 'API\UserController@register');
 
-Route::group(['middleware' => 'auth:api']), function (){
-Route::post('details', 'API\UserController@details');
-});
+);
 
